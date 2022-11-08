1d6eab7c8512439051e670f40d1ca894178b4fcb
Madeleine Lorås <madeleine.loras@gmail.com>
2022-11-08 19:34:55 +0100

    added svelte to project

---
+++ site/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>Hello world</div>
+</main>
+
+<style>
+  ´
+</style>
