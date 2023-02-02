04044d99ecb98ac82bb4b5867da319002d4f0a48
Madeleine <madeleine.loras@gmail.com>
2023-02-02 19:29:06 +0100

    sveltekit setup

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import Footer from "../components/Footer.svelte";
+    import PolishListPage from "../components/PolishListPage.svelte";
+    import Navbar from "../components/Navbar.svelte";
+    import sanityClient from "@sanity/client";
+
+  
+    const client = sanityClient({
+      projectId: "4lxm0vjb",
+      dataset: "production",
+      apiVersion: "2021-10-21",
+      token:
+        "skqm9XS6noUZ19tfG15kVWgEXWHo48T2m7vDQM1550cBiMrTxITkVmgPMEtNHF0uDcRJURf5kXbBFDyLjnBYjEHlUnvCgBhzTYiv02la0SEVH2EOdrvKqOh8DriN6cVPIxcUxtl7l9QC2VTcguYrVmXvrf1bES4dehLNSrOY1gfxDHS4Slij",
+      useCdn: false,
+    });
+  
+    export async function get() {
+      const data = await client.fetch(`*[_type == "nailpolish"]`);
+      if (data) {
+        return {
+          status: 200,
+          body: {
+            nailpolish: data,
+          },
+        };
+      }
+      return {
+        status: 500,
+        body: new Error("Internal Server Error"),
+      };
+    }
+  </script>
+  
+  <main>
+    <Navbar/>
+    {#await get()}
+      <p>...waiting</p>
+    {:then polish}
+    
+      <PolishListPage polishes={polish} />
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  
+    <Footer />
+  </main>
+  
